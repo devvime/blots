9f21fc51e7a50ecf8839179295595563c37244be
Victor Alves <victor.alves@leadforce.com.br>
2022-12-23 16:09:07 -0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './blots.js',
+  output: {
+    filename: 'blots.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+};
